e7f24c5a72a11b08320e00cf9591a72fbed6a89f
海 冯 <474875567@qq.com>
2019-08-21 22:38:17 +0800

    init

--- Java-v01/src/project_05/P_01.java
+++ Java-v01/src/project_05/P_01.java
@@ -1,4 +1,22 @@
 package project_05;
 
+/*
+--------------------------------------------------------------------------------------------------------------------------------------------------
+方法的初步使用————01————无参数类型
+--------------------------------------------------------------------------------------------------------------------------------------------------
+*/
 public class P_01 {
+    public static void main(String[] args) {
+        createEnemy();
+        createEnemy();
+        createEnemy();
+    }
+
+    public static void createEnemy() {
+        System.out.println("1.创建模型");
+        System.out.println("2.设置位置");
+        System.out.println("3.添加特效");
+        System.out.println("4.添加AI(人工智能)");
+        System.out.println();
+    }
 }
